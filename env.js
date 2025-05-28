57ea418a432ffbeeb119f0316740acc1d3c7a7d3
Lucía Seoane <luciaseo20@gmail.com>
2025-05-28 17:02:28 +0200

    Añadir base de datos y conexion con el servidor

---
+++ env.js
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+
+export const {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    MYSQL_PORT,
+    ADMIN_PASSWORD,
+    PORT,
+    SECRET,
+    SMTP_USER,
+    SMTP_API_KEY,
+    SMTP_HOST,
+    SMTP_PASS,
+    SMTP_PORT,
+} = process.env;
---
+++ src/database/getPool.js
@@ -0,0 +1,51 @@
+import mysql from 'mysql2/promise';
+// import generateErrorsUtils from '../utils/generateErrorsUtils.js';
+
+import {
+    MYSQL_HOST,
+    MYSQL_USER,
+    MYSQL_PASSWORD,
+    MYSQL_DATABASE,
+    MYSQL_PORT,
+} from '../../env.js';
+
+let pool;
+
+const getPool = async () => {
+    try {
+        if (!pool) {
+            const poolTemp = mysql.createPool({
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASSWORD,
+                port: MYSQL_PORT || 3306,
+                timezone: 'Z',
+            });
+
+            await poolTemp.query(
+                `
+                    CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}
+                `
+            );
+            pool = mysql.createPool({
+                connectionLimit: 10,
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASSWORD,
+                database: MYSQL_DATABASE,
+                port: MYSQL_PORT || 3306,
+                timezone: 'Z',
+            });
+        }
+
+        return await pool;
+    } catch (error) {
+        console.log(error);
+        // throw generateErrorsUtils(
+        //     'Error conectado a MySQL o no se encuentra la base de datos',
+        //     400
+        // );
+    }
+};
+
+export default getPool;
