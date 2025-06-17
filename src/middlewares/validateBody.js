b6d8d8908fbde8c10620d3732b147b3d57b0ed07
Adolfo Chaves <achlpz75@gmail.com>
2025-06-17 17:58:31 +0200

    adding changes hackathonsRouter

--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -2,6 +2,9 @@ const validateBody = (schema) => {
     return (req, res, next) => {
         const { error } = schema.validate(req.body, { abortEarly: false });
 
+        console.log(error);
+        
+
         if (error) {
             const messages = error.details.map((e) => e.message);
             return res.status(400).json({ error: messages });
--- src/router/hackathonsRouter.js
+++ src/router/hackathonsRouter.js
@@ -5,12 +5,12 @@ import {
     getHackathonByIdController,
 } from '../controllers/hackathons/index.js';
 
-import authMiddleware from '../middlewares/authMiddleware.js';
-import isAdminMiddleware from '../middlewares/authAdminMiddleware.js';
+
+
 
 const router = express.Router();
 
 router.get('/hackathons', listHackathonsController);
-router.get('/hackathons/:id', authMiddleware, getHackathonByIdController);
+router.get('/hackathons/:id', getHackathonByIdController);
 
 export default router;
